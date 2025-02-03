41fdb3ba635fdf6a54ce40499d7a83fc7835ba77
Dan Carpenter <drdcarpenter@googlemail.com>
2025-02-03 09:55:16 +0000

    sample data documentation

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Sample site boundary
+#'
+#' An example site boundary spatial dataset
+#'
+#' @format ## `sample_site_boundary`
+#' An sf object with 1 row and 8 columns:
+#' \describe{
+#'   \item{id}{ID}
+#'   \item{fid}{Feature ID}
+#'   \item{site_name}{Site name}
+#'   \item{proj_code}{Project code}
+#'   \item{sbi}{Single business identifier}
+#'   \item{area_ha}{Area in hectares}
+#'   \item{created_on}{Date item created}
+#'   \item{geometry}{Geometry}
+#'   ...
+#' }
+#' @source Digital Ecology Limited
+"sample_site_boundary"
